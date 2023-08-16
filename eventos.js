464520fe5c3dcca4043fa4ffc44955dd6a998ca5
antonelladutruel <antonelladutruel@gmail.com>
2023-08-16 14:35:50 -0300

    solucion-click

--- eventos.js
+++ eventos.js
@@ -1,3 +1,11 @@
-document.getElementById("div1").addEventListener("click", function() {
+const boton = document.getElementsByClassName("boton")[0]; 
+const div = document.getElementById("div1");
+
+boton.addEventListener("click", function(event) {
+    alert("Hola!");
+    event.stopPropagation();
+});
+
+div.addEventListener("click", function() {
     alert("Hola! Soy el div");
 });
